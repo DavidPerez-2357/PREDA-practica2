58ac7ade6d3b13f99fcd350b995eddd6ee8d68cf
david <davidyluni.personal@gmail.com>
2025-12-15 13:26:15 +0100

    ✨ Create class for an academic register

---
+++ src/main/java/main/AcademicRegister.java
@@ -0,0 +1,49 @@
+package main;
+
+public class AcademicRegister {
+    private Room room;
+    private Course course;
+    private Teacher teacher;
+
+    public int indexCourse;
+    public int indexRoom;
+    public int indexTeacher;
+
+    public AcademicRegister(Room room, Course course, Teacher teacher) {
+        this.room = room;
+        this.course = course;
+        this.teacher = teacher;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public boolean isValid() {
+        return teacher.canTeachCourse(course.getId()) && room.canHoldStudents(course.getNumStudents());
+    }
+
+    public String toString() {
+        return "AcademicRegister{room=" + room + ", course=" + course + ", teacher=" + teacher + "}";
+    }
+}
